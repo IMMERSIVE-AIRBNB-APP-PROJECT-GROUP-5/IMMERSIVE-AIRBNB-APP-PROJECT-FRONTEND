5ab1c094ede5978e17a42bfa258640b6e155c736
sitinuraisyaah <aisyah17717@gmail.com>
2023-06-11 16:18:04 +0700

    kerangka project

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CookiesProvider } from "react-cookie";
+
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import Profil from "./pages/Profil";
+import Detail from "./pages/Detail";
+import Sewakan from "./pages/Sewakan";
+import Disewakan from "./pages/Disewakan";
+import Riview from "./pages/Riview";
+
+const App = () => {
+  return (
+    <CookiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/Profil" element={<Profil />} />
+          <Route path="/Detail" element={<Detail />} />
+          <Route path="/Sewakan" element={<Sewakan />} />
+          <Route path="/Sewakan" element={<Disewakan />} />
+          <Route path="/Sewakan" element={<Riview />} />
+        </Routes>
+      </BrowserRouter>
+    </CookiesProvider>
+  );
+};
+
+export default App;
