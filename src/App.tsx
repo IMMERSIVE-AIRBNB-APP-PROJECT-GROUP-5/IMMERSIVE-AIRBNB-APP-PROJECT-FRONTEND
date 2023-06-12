ac5c8f0475caf7233f2126c64e815070d43404c3
Hilal Fadilah <hilalfadilah21@gmail.com>
2023-06-12 13:23:08 +0700

    add component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CookiesProvider } from "react-cookie";
 
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+// import Login from "./pages/Login";
+// import Signup from "./pages/Signup";
 import Home from "./pages/Home";
-import Profil from "./pages/Profil";
-import Detail from "./pages/Detail";
-import Sewakan from "./pages/Sewakan";
-import Disewakan from "./pages/Disewakan";
-import Riview from "./pages/Riview";
+// import Profil from "./pages/Profil";
+// import Detail from "./pages/Detail";
+// import Sewakan from "./pages/Sewakan";
+// import Disewakan from "./pages/Disewakan";
+// import Riview from "./pages/Riview";
 
 const App = () => {
   return (
@@ -17,13 +16,13 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/Home" element={<Home />} />
-          <Route path="/Login" element={<Login />} />
+          {/* <Route path="/Login" element={<Login />} />
           <Route path="/Signup" element={<Signup />} />
           <Route path="/Profil" element={<Profil />} />
           <Route path="/Detail" element={<Detail />} />
           <Route path="/Sewakan" element={<Sewakan />} />
           <Route path="/Sewakan" element={<Disewakan />} />
-          <Route path="/Sewakan" element={<Riview />} />
+          <Route path="/Sewakan" element={<Riview />} /> */}
         </Routes>
       </BrowserRouter>
     </CookiesProvider>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,68 @@
+import { FC } from "react";
+import logo from "../../public/Dashil.png";
+import avatar from "../../public/Avatar 1.png";
+
+interface ChildrenProps {
+  label?: string;
+}
+
+const Navbar: FC<ChildrenProps> = ({ label }) => {
+  return (
+    <div className="navbar bg-sky-500">
+      <div className="navbar-start">
+        <img src={logo} alt="Logo" className="w-[100px]" />
+        <h1 className="text-3xl text-white font-semibold">{label}</h1>
+      </div>
+      <div className="navbar-center">
+        <div className="form-control">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input max-w-xs lg:w-96"
+          />
+        </div>
+      </div>
+      <div className="navbar-end">
+        <div className="mx-5">
+          <button className="bg-white hover:bg-blue-500 text-sky-500 font-semibold hover:text-white py-2 px-4 border border-white hover:border-transparent rounded">
+            Login
+          </button>
+        </div>
+        <div className="mr-5">
+          <button className="bg-white hover:bg-blue-500 text-sky-500 font-semibold hover:text-white py-2 px-4 border border-white hover:border-transparent rounded">
+            Logout
+          </button>
+        </div>
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img src={avatar} />
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a className="justify-between">
+                Profile Saya
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Pesanan Saya</a>
+            </li>
+            <li>
+              <a>Sewakan</a>
+            </li>
+            <li>
+              <a>Penginapan Saya</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
